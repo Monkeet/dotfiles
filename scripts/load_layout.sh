6e274d6b887816648558fe392f08d42bae480971
Etienne St-Amant-Audet <etienne.st-amant-audet.1@ens.etsmtl.ca>
2019-01-15 16:47:12 -0500

    Load apps automatically when logging in

--- scripts/load_layout.sh
+++ scripts/load_layout.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-i3-msg "workspace 4 : " && (thunar &) && sleep 1
-i3-msg "workspace 2 : " && (gnome-terminal &) && (gnome-terminal &) && sleep 1
+i3-msg "workspace 1 : " && (chromium &) && disown
+i3-msg "workspace 3 : " && (thunderbird &) && disown
+i3-msg "workspace 10 : " && (spotify &) && disown
+sleep 2
+i3-msg "workspace 2 : " && (gnome-terminal &) && disown
+i3-msg "workspace 2 : " && (gnome-terminal &) && disown
+sleep 2
+i3-msg "workspace 4 : " && (thunar &) && disown
+sleep 2
 i3-msg "workspace 1 : "
