929935684b0c12c01faa25998308fc5cacf273a8
Etienne St-Amant-Audet <etienne.st-amant-audet.1@ens.etsmtl.ca>
2019-03-21 08:45:07 -0400

    Add autologin with lightdm

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,6 @@ sudo rsync -av applications/ /usr/share/applications/
 
 # Configs
 sudo cp ~/.dotfiles/config/30-touchpad.conf /etc/X11/xorg.conf.d/
-sudo cp ~/.dotfiles/config/90-xhc_sleep.rules /etc/udev/rules.d/
 sudo cp ~/.dotfiles/config/environment /etc/
 sudo cp ~/.dotfiles/config/logind.conf /etc/systemd/
 sudo cp ~/.dotfiles/config/vconsole.conf /etc/
@@ -30,6 +29,11 @@ sudo rsync -av ~/.dotfiles/services/ /etc/systemd/system/
 
 # Udev rules
 sudo rsync -av ~/.dotfiles/udev/ /etc/udev/rules.d/
+
+# Autologin
+sudo groupadd -r autologin
+sudo gpasswd -a etienne autologin
+sudo cp ~/.dotfiles/config/lightdm.conf /etc/lightdm/lightdm.conf
 
 # Games
 cp ~/.dotfiles/games/oldschool.prm /~/.config/runescape/share/prms/
